6ce5ddf5975b09f116d60460b00ad2c99bddea71
renaultluk <ren66.rl@gmail.com>
2024-05-09 23:17:15 +0800

    Finished drawing edges

--- src/components/MultiplexMap.js
+++ src/components/MultiplexMap.js
@@ -7,6 +7,7 @@ const MultiplexMap = (props) => {
   const centroidRef = useRef({});
   const tooltipRef = useRef();
   const titleRef = useRef();
+  const descriptionRef = useRef();
 
   useEffect(() => {
     const margin = { top: 30, right: 30, bottom: 70, left: 60 },
@@ -19,13 +20,20 @@ const MultiplexMap = (props) => {
 
       function drawMap(data) {
         centroidRef.current = {}
+
+        svg.selectAll(".taxi-zone")
+          .remove()
+        
+        svg.append("g")
+          .attr("class", "edges");
+
         svg.append("g")
           .selectAll("path")
           .data(data.features)
           .enter().append("path")
           .attr("class", "taxi-zone")
           .attr("d", path)
-          .attr("fill", "#444444")
+          .attr("fill", "rgba(68,68,68,0.5)")
           .attr("stroke-width", "1")
           .attr("stroke", "#888888")
           .on("mouseover", function (event, datum) {
@@ -36,61 +44,94 @@ const MultiplexMap = (props) => {
               .style("stroke", "#FF5555")
               .style("stroke-width", "2")
 
-            console.log(datum)
             tooltipRef.current.style.left = `${event.clientX + 8}px`;
             tooltipRef.current.style.top = `${event.clientY + 8}px`;
             tooltipRef.current.style.display = "flex";
-            titleRef.current.innerHTML = datum.properties.zone;
+            titleRef.current.innerHTML = `${datum.properties.location_id}. ${datum.properties.zone}`;
+            if (props.edges[datum.properties.location_id]) {
+              descriptionRef.current.style.display = "flex"
+              descriptionRef.current.innerHTML = `Degree: ${props.edges[datum.properties.location_id].length}\nConnected taxi zones: ${props.edges[datum.properties.location_id]}`
+            }
+            else {
+              descriptionRef.current.style.display = "none"
+            }
+
+            var node = datum.properties.location_id;
+
+            svg.select(".edges")
+              .selectAll(".connectedEdge")
+              .transition()
+              .duration("100")
+              .style("display", function(d) {
+                return (d.source == node || d.target == node) ? "block" : "none"; // Show edges that are connected to the hovered taxi zone
+              });
           })
           .on("mouseout", function (d, i) {
             d3.select(this)
               .transition()
               .duration("100")
-              .style("fill", "#444444")
+              .style("fill", "rgba(68,68,68,0.5)")
               .style("stroke", "#888888")
               .style("stroke-width", "1")
 
             tooltipRef.current.style.display = "none";
+
+            svg.select(".edges")
+            .selectAll(".connectedEdge")
+            .style("display", "block");
           })
           .each(function (d) {
-            // var bbox = this.getBoundingClientRect();
-            // centroidRef.current.push([(bbox.left + bbox.right) / 2, (bbox.top + bbox.bottom)])
-            // console.log(d)
-            centroidRef.current[d.properties.location_id] = path.centroid(d.geometry)
-          })
-          ;
+            centroidRef.current[d.properties.location_id] = myProjection.invert(path.centroid(d.geometry))
+          });
 
-          console.log(centroidRef.current)
+          
       }
 
-      // var link = []
-      //   Object.values(props.edges).forEach((node) => {
-      //     node.forEach(function(dest){
-      //       var source = centroidRef.current[node]
-      //       var target = centroidRef.current[dest.toString()]
-      //       console.log(source, target)
-      //       var topush = {type: "LineString", coordinates: [source, target]}
-      //       link.push(topush)
-      //     })
-      //   })
-
-      // console.log(link)
-
-      // svg
-      // .append("g")
-      // .selectAll("myPath")
-      // .data(link)
-      // .enter()
-      // .append("path")
-      //   .attr("class", "connectedEdge")
-      //   .attr("d", function(d){ return path(d)})
-      //   .style("fill", "none")
-      //   .style("stroke", "#69b3a2")
-      //   .style("stroke-width", 2)
-
-
       d3.json("/shapes.geojson").then(response => drawMap(response))
-  }, [props.edges]);
+    }, []);
+    
+    useEffect(() => {
+      console.log("edges changed, update")
+
+      const svg = d3.select(ref.current)
+      const myProjection = d3.geoMercator().scale(60000).translate([77800,47100])
+      const path = d3.geoPath().projection(myProjection)
+
+      svg.select(".edges").selectAll("path").remove();
+
+      var link = []
+        Object.keys(props.edges).forEach((node) => {
+          props.edges[node].forEach(function(dest){
+            var source = centroidRef.current[node];
+            var target = centroidRef.current[dest.toString()];
+            if (source !== undefined && target !== undefined) {
+              var topush = {
+                type: "LineString", 
+                coordinates: [source, target],
+                source: node,
+                target: dest.toString(),
+              }
+              link.push(topush)
+            }
+          })
+        })
+  
+        var edges = svg.select(".edges")
+          .selectAll("path")
+          .data(link)
+          .enter()
+          .append("path")
+          .attr("class", "connectedEdge")
+          .attr("d", function(d){ return path(d) })
+          .style("fill", "none")
+          .style("stroke", "#69b3a2")
+          .style("stroke-width", 1)
+
+        edges.raise()
+        
+          
+      
+  }, [props.edges])
 
   return (
     <>
@@ -102,6 +143,9 @@ const MultiplexMap = (props) => {
           border: "1px solid #888888",
           padding: 4,
           borderRadius: 4,
+          flexDirection: "column",
+          alignItems: "flex-start",
+          maxWidth: 200,
         }}
       >
         <span 
@@ -110,6 +154,15 @@ const MultiplexMap = (props) => {
             fontSize: 10,
             fontWeight: 500,
             color: "#FFFFFF",
+          }}
+        ></span>
+        <span
+          ref={descriptionRef}
+          style={{
+            fontSize: 9,
+            color: "#FFFFFF",
+            textAlign: "flex-start",
+            textWrap: "wrap",
           }}
         ></span>
       </div>
